4d8ed737bd26ed49af1fba92f459e329ba6e49d8
Ironpurush0 <ridamsaxena90.com>
2020-12-09 14:04:05 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+    type Query {
+        "A simple web app."
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+      hello: () => "Hello world",
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
