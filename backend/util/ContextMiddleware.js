fad154dafa121440f48b18b8c978b1e2c5df205e
Ironpurush0 <ridamsaxena90.com>
2021-01-20 23:23:20 +0530

    backend seems good.

--- backend/util/ContextMiddleware.js
+++ backend/util/ContextMiddleware.js
@@ -3,10 +3,12 @@ const {JWT_SECRET} = require('../config/env.json')
 const {AuthenticationError} = require('apollo-server')
 
 module.exports = context => {
-    if(context.req && context.req.headers.authorization){
+    if (context.req && context.req.headers.authorization) {
         const token = context.req.headers.authorization.split('Bearer ')[1]
         jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-            context.user = decodedToken
+          context.user = decodedToken
         })
-    }
+      }    
+      return context
+    
 }
